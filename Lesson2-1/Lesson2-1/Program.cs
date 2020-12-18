fa676a4a1f377fbeb51b9a616806d3d7a2059bfd
LidiiaMosina <downj@inbox.ru>
2020-12-18 19:07:46 +0300

    Lesson 2-1

---
+++ Lesson2-1/Lesson2-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите минимальную температуру ");
+            double tmin = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите максимальную температуру ");
+            double tmax = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Средняя температура {(tmin + tmax) / 2}");
+            Console.ReadLine();
+        }
+    }
+}
