a9b75f30430f4180e73abee4944f6abdf5425a35
LidiiaMosina <downj@inbox.ru>
2020-12-18 19:27:20 +0300

    Lesson 2-3

--- Lesson2-3/Lesson2-3/Program.cs
+++ Lesson2-3/Lesson2-3/Program.cs
@@ -10,6 +10,18 @@ namespace Lesson2_3
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите число ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if ((num%2)>0) 
+                    {
+                Console.WriteLine("Число не четное");
+
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+            Console.ReadLine();
         }
     }
 }
