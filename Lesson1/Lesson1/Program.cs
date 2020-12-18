68312c13ad776897e1af788939d3c12272451f25
LidiiaMosina <downj@inbox.ru>
2020-12-18 19:06:30 +0300

    Lesson1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            DateTime now = DateTime.Now;
+            var date = now.GetDateTimeFormats('d')[0];
+            Console.WriteLine($"Hello, {name}, today is {date}!");
+            Console.ReadLine();
+        }
+    }
+}
