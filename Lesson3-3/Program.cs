70de3251d5472097d23426f1c82b92941c631b8b
LidiiaMosina <downj@inbox.ru>
2020-12-22 19:12:17 +0300

    Lesson3-3

---
+++ Lesson3-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            for (int i = greeting.Length-1; i >= 0; i--)
+            {
+                Console.Write(greeting[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
