f78794f4e4a572fa76b7f795f0cf73cf96e2918b
LidiiaMosina <downj@inbox.ru>
2020-12-22 17:30:13 +0300

    Lesson-3-1

---
+++ Lesson3-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
