8dc2d7304fb1b1a965fd0327affbfec1c86f9588
LidiiaMosina <downj@inbox.ru>
2020-12-25 18:39:35 +0300

    Lesson4-2

---
+++ Lesson4-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку чисел");
+            string strNum = Console.ReadLine();
+            String[] words = strNum.Split(' ');
+            int sum=0;
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                sum = sum+Convert.ToInt32(words[i]);
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+
+    }
+}
